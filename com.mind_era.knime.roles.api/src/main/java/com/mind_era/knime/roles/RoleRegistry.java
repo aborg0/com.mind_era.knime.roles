5cacef98e9e0854b754a016f3536eb98b683a6a0
Bakos, Gabor <bakos.gabor@mind-era.com>
2013-05-01 23:31:06 +0200

    Initial commit of the roles api project.
    This commit includes the project with interfaces and the extension
    points.

---
+++ com.mind_era.knime.roles.api/src/main/java/com/mind_era/knime/roles/RoleRegistry.java
@@ -0,0 +1,13 @@
+/* Copyright © 2013 Mind Eratosthenes Kft.
+ * Licence: http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.mind_era.knime.roles;
+
+/**
+ * The role registry class will be offered as a service to perform various role specific tasks, checks.
+ * 
+ * @author Gabor Bakos
+ */
+class RoleRegistry {
+
+}
---
+++ com.mind_era.knime.roles.api/src/main/java/com/mind_era/knime/roles/internal/Activator.java
@@ -0,0 +1,38 @@
+/* Copyright © 2013 Mind Eratosthenes Kft.
+ * Licence: http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.mind_era.knime.roles.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Bundle activator.
+ * 
+ * @author Gabor Bakos
+ */
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
